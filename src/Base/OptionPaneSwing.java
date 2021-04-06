dacd9a737ca0041a0604d0d8f98c3abb6c03f033
loris_sav8h55 <loris_sav8h55@LDLC-Loris>
2021-04-06 20:23:05 +0200

    Ajout de la gestion d'erreurs

--- src/Base/OptionPaneSwing.java
+++ src/Base/OptionPaneSwing.java
@@ -6,70 +6,74 @@ import javax.swing.JOptionPane;
 
 public class OptionPaneSwing extends JFrame {
 	private static final long serialVersionUID = 4038914321129666366L;
+	
+	final static String programName = "Pythagore";
 
+	/**
+	 * Fonction invoquant une fenêtre d'erreur avec un message personnalisé
+	 * @author Lorissou_
+	 * @param message Message affiché dans la boîte de dialogue
+	 */
+	
 	public static void showDialog(String message) {
-		
-		/**
-		 * Fonction invoquant une fenêtre d'erreur avec un message personnalisé
-		 * @author Lorissou_
-		 * @param message Message affiché dans la boîte de dialogue
-		 */
 		
 		showMessagePane(message, "Erreur", 2, null);
 		
 	}
 	
+	/**
+	 * Fonction invoquant une fenêtre d'erreur avec un message personnalisé
+	 * @author Lorissou_
+	 * @param message Message affiché dans la boîte de dialogue
+	 * @param titre Titre de la boîte de dialogue
+	 */
+	
 	public static void showDialog(String message, String titre) {
-		
-		/**
-		 * Fonction invoquant une fenêtre d'erreur avec un message personnalisé
-		 * @author Lorissou_
-		 * @param message Message affiché dans la boîte de dialogue
-		 * @param titre Titre de la boîte de dialogue
-		 */
 		
 		showMessagePane(message, titre, 2, null);
 		
 	}
 	
+	/**
+	 * Fonction invoquant une fenêtre de dialogue avec un message personnalisé
+	 * @author Lorissou_
+	 * @param message Message affiché dans la boîte de dialogue
+	 * @param titre Titre de la boîte de dialogue
+	 * @param messageType Type de message WARNING / INFORMATION / ...
+	 */
+	
 	public static void showDialog(String message, String titre, int messageType) {
-		
-		/**
-		 * Fonction invoquant une fenêtre de dialogue avec un message personnalisé
-		 * @author Lorissou_
-		 * @param message Message affiché dans la boîte de dialogue
-		 * @param titre Titre de la boîte de dialogue
-		 */
 		
 		showMessagePane(message, titre, messageType, null);
 		
 	}
 	
-	public static void showDialog(String message, String titre, int messageType, Icon icone) {
+	/**
+	 * Fonction pour générer des messages fatal dans une OptionPane de swing
+	 * @author Lorissou_
+	 * @param message Message personnalisé dans la zone de texte
+	 */
+	
+	public static void showFatalError(String message) {
+	
+		showMessagePane(message, "Erreur fatale", 0, null);
 		
-		/**
-		 * Fonction invoquant une fenêtre de dialogue avec un message personnalisé
-		 * @author Lorissou_
-		 * @param message Message affiché dans la boîte de dialogue
-		 * @param titre Titre de la boîte de dialogue
-		 */
-		
-		showMessagePane(message, titre, messageType, icone);
+		System.exit(1);
 		
 	}
 	
+	/**
+	 * Fonction invoquant une fenêtre OptionPane de message, avec un message, titre, code et icone de défaut personnalisé
+	 * @author Loris
+	 * @param message Message affiché dans la boîte de dialogue
+	 * @param titre Titre de la boîte de dialogue
+	 * @param messageType Personnalisation de l'image de défaut dans la boîte de dialogue
+	 * @param icone Icone de la boîte de dialogue
+	 */
+	
 	public static void showMessagePane (String message, String titre, int messageType, Icon icone) {
 		
-		/**
-		 * Fonction invoquant une fenêtre OptionPane de message, avec un message, titre, code et icone de défaut personnalisé
-		 * @author Loris
-		 * @param message Message affiché dans la boîte de dialogue
-		 * @param titre Titre de la boîte de dialogue
-		 * @param messageType Personnalisation de l'image de défaut dans la boîte de dialogue
-		 * @param icone Icone de la boîte de dialogue
-		 */
-		
-		JOptionPane.showMessageDialog(new JFrame(), message, titre, messageType, icone);
+		JOptionPane.showMessageDialog(new JFrame(), message, programName + titre, messageType, icone);
 		
 	}
 }
--- src/Base/TopLevelWindow.java
+++ src/Base/TopLevelWindow.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
+import Old.Utils;
+
 /**
  * <h1>Chiffres premiers</h1>
  * <p>Programme dédié au calcul des chiffres premiers<p>
@@ -33,16 +35,6 @@ public class TopLevelWindow {
 		
 		JButton calculateButton = new JButton("Calculer");
 		frame.add(calculateButton);
-		calculateButton.addKeyListener(new KeyListener() {
-			@Override
-			public void keyPressed(KeyEvent e) {}
-			@Override
-			public void keyTyped(KeyEvent e) {}
-			@Override
-			public void keyReleased(KeyEvent e) {
-				calculate(valeur_aField, valeur_bField, valeur_cField);
-			}
-		});
 		calculateButton.addActionListener(new ActionListener() { // Déclaration de l'action à réaliser lors de l'appui du bouton
 			
 			@Override
@@ -50,6 +42,17 @@ public class TopLevelWindow {
 				calculate(valeur_aField, valeur_bField, valeur_cField);
 			}
 			
+		});
+		
+		// Bouton effacer les champs
+		JButton effacerButton = new JButton();
+		frame.add(effacerButton);
+		effacerButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
 		});
 		
 		//Rendre visible l'interface
@@ -74,53 +77,83 @@ public class TopLevelWindow {
 		if (b.getText().isEmpty() == true) {numberOfEmpty.add("b");}
 		if (c.getText().isEmpty() == true) {numberOfEmpty.add("c");}
 		
-		double a_val;
-		double b_val;
-		double c_val;
+		double a_val = 0;
+		double b_val = 0;
+		double c_val = 0;
 		
-		if (a.getText().isEmpty() == true && b.getText().isEmpty() == true && c.getText().isEmpty() == true) {
+		try {
 			
-			OptionPaneSwing.showDialog("Veuillez remplir tous les champs...", "Pythagore -  Erreur", 2);
+			System.out.println("Demande avec [" + numberOfEmpty.size() + "] de case(s) libre " + numberOfEmpty);
+			System.out.println("Demande avec a=" + a_val + " b=" + b_val + " c=" + c_val);
 			
-		}
-		if (a.getText().isEmpty() == false && b.getText().isEmpty() == false && c.getText().isEmpty() == true) {
+			// Si 1 case de remplie
+			if (numberOfEmpty.size() == 2) { OptionPaneSwing.showDialog("Veuillez remplir au minimum 2 champs", "Erreur utilisateur", 0); }
 			
-			// a & b != null ; c == null
+			// Si 3 cases de remplies --> calculer si bon ou pas
+			else if (numberOfEmpty.size() == 0) {
+				
+				a_val = Double.parseDouble(a.getText());
+				b_val = Double.parseDouble(b.getText());
+				c_val = Double.parseDouble(c.getText());
+				
+				if (Math.ceil(Math.pow(a_val, 2) + Math.pow(b_val, 2)) == Math.ceil(Math.pow(c_val, 2))) {System.out.println("Hello");}
+				
+				OptionPaneSwing.showDialog("Calcul", "", 1);
+				
+				}
 			
-			a_val = Double.parseDouble(a.getText());
-			b_val = Double.parseDouble(b.getText());
+			// Si 2 cases de remplies
+			else if (numberOfEmpty.size() == 1) { 
 			
-			c_val = Math.sqrt(Math.pow(a_val, 2) + Math.pow(b_val, 2));
+				if (a.getText().isEmpty() == true && b.getText().isEmpty() == true && c.getText().isEmpty() == true) {
+					OptionPaneSwing.showDialog("Veuillez remplir tous les champs...", "Erreur", 2);
+				}
+				if (a.getText().isEmpty() == false && b.getText().isEmpty() == false && c.getText().isEmpty() == true) {
+					
+					// a & b != null ; c == null
+					a_val = Double.parseDouble(a.getText());
+					b_val = Double.parseDouble(b.getText());
+					
+					c_val = Math.sqrt(Math.pow(a_val, 2) + Math.pow(b_val, 2));
+					
+					if (c_val % 1 != 0) {c.setText(String.valueOf(c_val));}
+					else if (c_val % 1 == 0) {c.setText(String.valueOf((int) c_val));}
+					else {OptionPaneSwing.showFatalError("Erreur fatale");}
+				
+				} else if (a.getText().isEmpty() == false && b.getText().isEmpty() == true && c.getText().isEmpty() == false) {
+					
+					// a & c != null ; b == null
+					
+					a_val = Double.parseDouble(a.getText());
+					c_val = Double.parseDouble(c.getText());
+					
+					b_val = Math.sqrt(Math.pow(c_val, 2) - Math.pow(a_val, 2));
+					
+					if (b_val % 1 != 0) {b.setText(String.valueOf(b_val));}
+					else if (b_val % 1 == 0) {b.setText(String.valueOf((int) b_val));}
+					else {OptionPaneSwing.showFatalError("Erreur fatale");}
+				
+				} else if (a.getText().isEmpty() == true && b.getText().isEmpty() == false && c.getText().isEmpty() == false) {
+					
+					// b & c != null ; a == null
+					
+					b_val = Double.parseDouble(b.getText());
+					c_val = Double.parseDouble(c.getText());
+					
+					a_val = Math.sqrt(Math.pow(c_val, 2) - Math.pow(b_val, 2));
+					
+					if (a_val % 1 != 0) {a.setText(String.valueOf(a_val));}
+					else if (a_val % 1 == 0) {a.setText(String.valueOf((int) a_val));}
+					else {OptionPaneSwing.showFatalError("Erreur fatale");}
+				}
+				
+			} 
 			
-			if (c_val % 1 != 0) {c.setText(String.valueOf(c_val));}
-			else if (c_val % 1 == 0) {c.setText(String.valueOf((int) c_val));}
-			else {OptionPaneSwing.showDialog("Erreur fatale dans la fonction \"calculer\" du programme", "Pythagore - Erreur fatale");}
-		
-		} else if (a.getText().isEmpty() == false && b.getText().isEmpty() == true && c.getText().isEmpty() == false) {
+			// Si cases en erreur
+			else if (numberOfEmpty.size() > 3 || numberOfEmpty.size() < -1) {OptionPaneSwing.showFatalError("Erreur fatale"); }
 			
-			// a & c != null ; b == null
-			
-			a_val = Double.parseDouble(a.getText());
-			c_val = Double.parseDouble(c.getText());
-			
-			b_val = Math.sqrt(Math.pow(c_val, 2) - Math.pow(a_val, 2));
-			
-			if (b_val % 1 != 0) {b.setText(String.valueOf(b_val));}
-			else if (b_val % 1 == 0) {b.setText(String.valueOf((int) b_val));}
-			else {OptionPaneSwing.showDialog("Erreur fatale dans la fonction \"calculer\" du programme", "Pythagore - Erreur fatale");}
-		
-		} else if (a.getText().isEmpty() == true && b.getText().isEmpty() == false && c.getText().isEmpty() == false) {
-			
-			// b & c != null ; a == null
-			
-			b_val = Double.parseDouble(b.getText());
-			c_val = Double.parseDouble(c.getText());
-			
-			a_val = Math.sqrt(Math.pow(c_val, 2) - Math.pow(b_val, 2));
-			
-			if (a_val % 1 != 0) {a.setText(String.valueOf(a_val));}
-			else if (a_val % 1 == 0) {a.setText(String.valueOf((int) a_val));}
-			else {OptionPaneSwing.showDialog("Erreur fatale dans la fonction \"calculer\" du programme", "Pythagore - Erreur fatale");}
+		} catch (NumberFormatException e) {
+			OptionPaneSwing.showDialog("Veuillez insérer uniquement des caractères numériques...", "Erreur syntaxe", 0);
 		}
 		
 	}
